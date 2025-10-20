29fc82b00a49303d217faee85f9d559ddc5d36bf
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-10-20 12:54:46 +0530

    Setup Cars app with listings, model, and navigation updates

--- Motormart/settings.py
+++ Motormart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'cars.apps.CarsConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'Motormart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
