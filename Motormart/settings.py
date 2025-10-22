f97c91efa66d7d67fbdb64506e80e476d3b7da0d
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-10-22 15:18:21 +0530

    Single car view, cars page, and pagination setup

--- Motormart/settings.py
+++ Motormart/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Motormart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
