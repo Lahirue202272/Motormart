f87959a9223b88346ddeca50183382afd73fb7b5
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-11-15 15:24:19 +0530

    Create dashboard functionality and make contact page and send emails

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Team
 from cars.models import Car
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
@@ -32,6 +36,28 @@ def about(request):
     return render(request, 'pages/about.html',data)
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        email_subject = f"New Contact Form Submission: {subject}"
+        message_body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\n\nMessage:\n{message}"
+
+        admin_info = User.objects.get(is_superuser=True)
+        admin_email = admin_info.email
+        send_mail(
+            email_subject,
+            message_body,
+            "balagamageniroshan@gmail.com",
+            [admin_email],
+            fail_silently=False,
+        )
+        messages.success(request, 'Thank you for contacting us. We will get back to you shortly.')
+        return redirect('contact')
+
+        # Here you can handle the form data, e.g., save it to the database or send an email
     return render(request, 'pages/contact.html')
 
 def services(request):
